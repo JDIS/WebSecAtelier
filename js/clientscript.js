56b628b7d213587dfee362a641fad608fee7a9aa
carapas <david_lalancette_5@hotmail.com>
2016-09-09 12:20:47 -0400

    Obfuscate

--- js/clientscript.js
+++ js/clientscript.js
@@ -1,11 +1,5 @@
 function cc(n,v,d) {
-    if (d) {
-        var date = new Date();
-        date.setTime(date.getTime()+(d*24*60*60*1000));
-        var expires = "; expires="+date.toGMTString();
-    }
-    else var expires = "";
-    document.cookie = n+"="+v+expires+"; path=/";
+    eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('8(d){2 1=9 7();1.6(1.4()+(d*5*3*3*a));2 0="; 0="+1.g()}f 2 0="";b.c=e+"="+h+0+"; i=/";',19,19,'expires|date|var|60|getTime|24|setTime|Date|if|new|1000|document|cookie||n|else|toGMTString|v|path'.split('|'),0,{}))
 }
 
 
@@ -13,5 +7,5 @@ $('document').ready(function(){
 	$.get( "js/request.txt", function( data ) {
 	});
 
-	cc("supersecret", "test", undefined);
+	cc("supersecret", "ZmxhZy1jb29raWVzYXJlcmVhbGx5aW1wb3J0YW50", undefined);
 });
